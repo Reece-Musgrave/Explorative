944d7db830df7bcce7298bb9e105317913beb9ee
Reece Musgrave <reecemus1999@gmail.com>
2025-12-07 20:31:55 +0000

    Removing comments from test files

--- tests/backend/services/test_maze_service.py
+++ tests/backend/services/test_maze_service.py
@@ -3,10 +3,9 @@ import pytest
 
 
 def test_retrieve_show_returns_show_id(requests_mock):
-    #Arrange 
     api = ShowAPI()
     example_show = "The Walking Dead"
-    #Act
+    
     requests_mock.get(
         f"{api.base_url}/singlesearch/shows?q={example_show}",
         json={
@@ -16,86 +15,80 @@ def test_retrieve_show_returns_show_id(requests_mock):
         },
         status_code=200,
     )
-    #Assert
+    
     result = api.retrieve_show(example_show)
     assert result == (123, "The Walking Dead", "http://image.url")
 
 def test_retrieve_show_returns_exception(requests_mock):
-    #Arrange 
     api = ShowAPI()
     example_show = "example show "
-    #Act
+    
     requests_mock.get(
         f"{api.base_url}/singlesearch/shows?q={example_show}",
         status_code=404,
     )
-    #Assert
+    
     with pytest.raises(APIError) as excinfo:
         api.retrieve_show(example_show)    
 
 def test_retrieve_number_of_seasons_returns_number_of_seasons(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_show_id = "30082"
-    #Act
+    
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/seasons",
         json=[{"id": 1}, {"id": 2}, {"id": 3}],
         status_code=200,
     )
-    #Assert
+    
     result = api.retrieve_number_of_seasons(example_show_id)
     assert result == (3)
 
 def test_retrieve_number_of_seasons_returns_exception(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_show_id = "00000"
-    #Act
+    
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/seasons",
         status_code=404,
     )
-    #Assert
+    
     with pytest.raises(APIError) as excinfo:
         api.retrieve_number_of_seasons(example_show_id)
 
 def test_retrieve_number_of_episodes_returns_episodes(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_show_id = "30010"
     example_season_number = 1
-    #Act
+    
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/seasons",
         json=[{"id": 1, "episodeOrder": 8}],
         status_code = 200
     )
-    #Assert
+    
     result = api.retrieve_number_of_episodes(example_show_id, example_season_number)
     assert result == 8
 
 def test_retrieve_number_of_episodes_returns_exception(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_show_id = "00000"
     example_season_number = 0
-    #Act
+
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/seasons",
         status_code=404
     )
-    #Assert
+    
     with pytest.raises(APIError) as excinfo:
         api.retrieve_number_of_episodes(example_show_id, example_season_number)
 
 def test_return_episode_returns_episode_data(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_episode_id = 10
     example_show_id = "1000"
     example_season_id = 3
-    #Act
+
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/episodebynumber?season={example_season_id}&number={example_episode_id}",
         json={
@@ -106,21 +99,20 @@ def test_return_episode_returns_episode_data(requests_mock):
         },
         status_code=200
     )
-    #Assert
+    
     result = api.retrieve_episode(example_episode_id, example_show_id, example_season_id)
     assert result["season_ID"] == 3
 
 def test_return_episode_returns_exception(requests_mock):
-    #Arrange
     api = ShowAPI()
     example_episode_id = 10
     example_show_id = "1000"
     example_season_id = 3
-    #Act
+
     requests_mock.get(
         f"{api.base_url}/shows/{example_show_id}/episodebynumber?season={example_season_id}&number={example_episode_id}",
         status_code=404
     )
-    #Assert
+    
     with pytest.raises(APIError) as excinfo:
         api.retrieve_episode(example_episode_id, example_show_id, example_season_id)
