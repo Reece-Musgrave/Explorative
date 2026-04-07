f9380e01e89df7ef39eaf443789e5c7497f4ce03
Reece Musgrave <reecemus1999@gmail.com>
2026-04-07 10:50:18 +0100

    feat(backend): insert_post method created

---
+++ src/backend/services/posts_service.py
@@ -0,0 +1,33 @@
+from backend.core.exceptions import NotFoundError
+from sqlalchemy.orm import Session
+from backend.models.shows import Shows
+from backend.models.seasons import Seasons
+from backend.models.episodes import Episodes
+from backend.models.posts import Posts
+
+def insert_post(db: Session, message: str, username: str, timestamp: str, show_name: str, season_number: int, episode_number: int, post_type: str):
+    episode = (
+        db.query(Episodes)
+        .join(Episodes.seasons)
+        .join(Seasons.shows)
+        .filter(Shows.name == show_name)
+        .filter(Seasons.season_number == season_number)
+        .filter(Episodes.episode_number == episode_number)
+        .first()
+    )
+    if not episode:
+        raise NotFoundError(f"Episode S{season_number}E{episode_number} of {show_name} not found in database")
+
+    new_post = Posts(
+        episode_id = episode.id,
+        message = message,
+        username = username,
+        timestamp = timestamp,
+        post_type = post_type
+    )
+    db.add(new_post)
+    db.commit()
+    
+
+def retrieve_post():
+    pass
